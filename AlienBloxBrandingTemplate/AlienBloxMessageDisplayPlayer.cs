23e77bd808e7b4a3d9da09efc55ad7eebab06b44
AlienBlox <alienthebest123@outlook.com>
2025-11-18 15:23:06 +0000

    text

---
+++ AlienBloxBrandingTemplate/AlienBloxMessageDisplayPlayer.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria.ModLoader;
+
+namespace Discordya.AlienBloxBrandingTemplate
+{
+	public class AlienBloxMessageDisplayPlayer : ModPlayer
+	{
+		public override void OnEnterWorld()
+		{
+			this.QuickLocalizedTextDisplay("MessageAlienBlox.Message1", Color.SteelBlue);
+			this.QuickLocalizedTextDisplay("MessageAlienBlox.Message2", Color.LightSteelBlue);
+			this.QuickLocalizedTextDisplay("MessageAlienBlox.Message3", Color.LightSteelBlue);
+			this.QuickLocalizedTextDisplay("MessageAlienBlox.Message4", Color.LightSteelBlue);
+			this.QuickLocalizedTextDisplay("MessageAlienBlox.Message5", Color.LightSteelBlue);
+			this.QuickLocalizedTextDisplay("MessageAlienBlox.Message6", Color.LightSteelBlue);
+		}
+	}
+}
---
+++ AlienBloxBrandingTemplate/QuickTextDisplay.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+
+namespace Discordya.AlienBloxBrandingTemplate
+{
+	/// <summary>
+	/// Utility class made by AlienBlox, not part of the default Discord Rich Presence Mod,
+	/// Used to quickly display text
+	/// </summary>
+	public static class QuickTextDisplay
+	{
+		/// <summary>
+		/// Quick and dirty displays texts
+		/// </summary>
+		/// <param name="o">For the extension to work</param>
+		/// <param name="key">The locale key</param>
+		public static void QuickLocalizedTextDisplay(this object o, string key, Color? TextColor = null)
+		{
+			if (TextColor.HasValue)
+			{
+				Main.NewText(Terraria.Localization.Language.GetOrRegister($"Mods.Discordya.{key}"), TextColor);
+			}
+			else
+			{
+				Main.NewText(Terraria.Localization.Language.GetOrRegister($"Mods.Discordya.{key}"));
+			}
+
+			DiscordyaMod._discordyaMod.Logger.Warn(o);
+		}
+	}
+}
