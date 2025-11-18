686f6654be9b509a40c20d8f5dcfd7def27efdd2
AlienBlox <alienthebest123@outlook.com>
2025-11-18 15:55:53 +0000

    fixed probably

---
+++ AlienBloxBrandingTemplate/InfoItem.cs
@@ -0,0 +1,15 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Discordya.AlienBloxBrandingTemplate
+{
+	public class InfoItem : ModItem, ILocalizedModType
+	{
+		public new string LocalizationCategory => "Items";
+
+		public override void SetDefaults()
+		{
+			Item.rare = ItemRarityID.Blue;
+		}
+	}
+}
